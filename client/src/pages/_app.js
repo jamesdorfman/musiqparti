94406ece1a9624f52b8241168bfadbabc8a8a631
Rickson Yang <rixin.yang.2001@gmail.com>
2021-07-04 02:17:47 -0400

    added basic login page + query calls to backend

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,19 +1,24 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-import theme from '../theme'
+import theme from "../theme";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </QueryClientProvider>
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ client/src/pages/login.js
@@ -0,0 +1,99 @@
+import { Link as ChakraLink, Flex, Button, Heading } from "@chakra-ui/react";
+import { Container } from "../components/Container";
+import axios from "axios";
+import { useState } from "react";
+
+const Index = () => {
+  const [user, setUser] = useState({});
+
+  console.log(user);
+
+  return (
+    <Container height="100vh">
+      <Flex justifyContent="center" alignItems="center">
+        <Heading
+          fontSize="10vw"
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgClip="text"
+        >
+          Login With Spotify
+        </Heading>
+      </Flex>
+      <Flex>
+        <ChakraLink m={4} href="http://localhost:5000/spotify/login">
+          <Button as={ChakraLink}>Login</Button>
+        </ChakraLink>
+        <Button
+          m={4}
+          onClick={async () => {
+            const { data } = await axios.get(
+              "http://localhost:5000/spotify/hello",
+              {
+                withCredentials: true,
+              }
+            );
+            setUser(data);
+          }}
+        >
+          Get Info
+        </Button>
+        {user.id ? (
+          <Button
+            m={4}
+            onClick={async () => {
+              await axios.get("http://localhost:5000/spotify/logout", {
+                withCredentials: true,
+              });
+              setUser({});
+            }}
+          >
+            Logout
+          </Button>
+        ) : (
+          <></>
+        )}
+      </Flex>
+
+      {user.id ? (
+        <>
+          <h1>Logged in as {user.display_name}</h1>
+          <div>
+            <div>
+              <img width="150" src={user.images[0].url} />
+            </div>
+            <div>
+              <dl>
+                <dt>Display name</dt>
+                <dd>{user.display_name}</dd>
+                <dt>Id</dt>
+                <dd>{user.id}</dd>
+                <dt>Email</dt>
+                <dd>{user.email}</dd>
+                <dt>Spotify URI</dt>
+                <dd>
+                  <a href={user.external_urls.spotify}>
+                    {user.external_urls.spotify}
+                  </a>
+                </dd>
+                <dt>Link</dt>
+                <dd>
+                  <a href={user.href}>{user.href}</a>
+                </dd>
+                <dt>Profile Image</dt>
+                <dd>
+                  <a href={user.images[0].url}>{user.images[0].url}</a>
+                </dd>
+                <dt>Country</dt>
+                <dd>{user.country}</dd>
+              </dl>
+            </div>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+    </Container>
+  );
+};
+
+export default Index;
