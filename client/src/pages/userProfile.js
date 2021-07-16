e07d8ae832a1dd1bb83e44aa3d261357ec5f4218
Anna Wang <annaw2193@gmail.com>
2021-07-16 18:36:30 -0400

    formatting changes

--- client/src/pages/userProfile.js
+++ client/src/pages/userProfile.js
@@ -61,7 +61,8 @@ class ProfilePic extends React.Component {
             <VStack>
                 <Box>
                     <Heading
-                    fontSize="3vw"
+                    fontWeight={600}
+                    fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
                     bgGradient="linear(to-l, #7928CA, #FF0080)"
                     bgClip="text">
                         {profile.display_name}
@@ -94,6 +95,22 @@ function LikeButton(props) {
           </Button>
       );
 }
+
+
+
+// function LikeButton(props) {
+//     const handleClick = () => 
+//     {{props.changeIncrement();
+//      props.changeCounter(props.liked);}};
+//         return (
+//           <Button 
+//               icon="star"
+//               onClick={handleClick}
+//               bgGradient={props.color[{{props.liked}}]}>
+//               {props.liked}
+//           </Button>
+//       );
+// }
   
 function DisplayLikes(props) {
       return (
@@ -105,12 +122,15 @@ function DisplayLikes(props) {
 function Likes(props) {
       const [counter, setCounter] = useState(100); // currently hardcoded find way to retrieve the user's likes
       const [increment, setIncrement] = useState(1);
+      let key1 = 1;
+      let key2 = -1;
+      const buttonColor= ['linear(to-l, #7928CB, #FF0080)', 'linear(to-l, #7928CA, #FF0080)'];
       const toggleLike = () => setIncrement(-1 * increment);
       const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
       return(
           <div>
               <HStack>
-                <LikeButton changeIncrement={toggleLike} changeCounter={incrementCounter} liked={increment} currentLikes={counter} />
+                <LikeButton changeIncrement={toggleLike} changeCounter={incrementCounter} liked={increment} currentLikes={counter} color={buttonColor}/>
                 <VStack>
                     <Box><DisplayLikes message={counter}/></Box>
                     <Box><h1>LIKES</h1></Box>
